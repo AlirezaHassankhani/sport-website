bc9da324f510ac2cd8349f26e05fb051f6b9635e
flatrov <flatrov1@gmail.com>
2025-08-03 17:11:39 +0330

    complate slider section

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,25 @@
 import "./style.css";
+import Swiper from "swiper";
+import "swiper/css";
+import "swiper/css/pagination"
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
 
-
+const sports = new Swiper(".sports", {
+  modules: [Navigation, Pagination, Autoplay],
+  autoplay: {
+    delay: 4000,
+  },
+  loop: true,
+  pagination: {
+    el: ".custom-pagination",
+    clickable: true,
+    renderBullet: function(index, classList) {
+        console.log(classList)
+        return `<span class="${classList}"></span>`
+    }
+  },
+  navigation: {
+    prevEl: ".stoprt-slide-prev",
+    nextEl: ".stoprt-slide-next",
+  },
+});
